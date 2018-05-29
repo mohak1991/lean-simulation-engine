7e8d48277eef3a6071e3e33f58dda7d53bac0ce2
Rachna Sharma <RSharma34@slb.com>
2018-05-29 15:06:58 +0530

    First commit

---
+++ src/main/java/com/lean/LeanSimulationEngineApplication.java
@@ -0,0 +1,12 @@
+package com.lean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanSimulationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanSimulationEngineApplication.class, args);
+	}
+}
